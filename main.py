fd4c739d76786ab234880258342d4deab4e5b074
ahalllllla11111-droid <ahalllllla11111@gmail.com>
2026-06-01 20:38:23 -0700

    Update main.py

--- main.py
+++ main.py
@@ -89,7 +89,7 @@ st.title("💍 دَعْوَةُ حَفْلِ زِفَافٍ 💍")
 
 st.markdown("<p style='font-style: italic; color: #555555; font-size: 18px; font-weight: bold;'>\"وَمِنْ آيَاتِهِ أَنْ خَلَقَ لَكُم مِّنْ أَنفُسِكُمْ أَزْوَاجًا لِّتَسْكُنُوا إِلَيْهَا وَجَعَلَ بَيْنَكُم مَّوَدَّةً وَرَحْمَةً\"</p>", unsafe_allow_html=True)
 
-st.header("احمد & فاطمة❤️")
+st.markdown("<h2 style='text-align: center; color: #8A6D3B; font-family: Cairo, sans-serif; font-weight: bold;'>💖 احمد & فاطمة 💖</h2>", unsafe_allow_html=True)
 
 st.markdown("<hr style='border-top: 1px solid #dfc48c;'>", unsafe_allow_html=True)
 st.subheader("بإذن الله تعالى، يسعدنا ويشرفنا دعوتكم لمشاركتنا فرحة العمر")
